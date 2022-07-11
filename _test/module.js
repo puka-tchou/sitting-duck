44b263c06518e3a81a5a0418b3678ef00db9cf04
Gaspard [puka] <gaspard.bariller-drouet@sogefi-sig.com>
2022-07-11 10:02:01 +0200

    chore(release): v1.1.0

--- _test/module.js
+++ _test/module.js
@@ -1,2 +1,9 @@
 // @MODULE
-// fuuuuu
+import { distance, point } from "@turf/turf";
+
+const from = point([-75.343, 39.984]);
+const to = point([-75.534, 39.123]);
+const options = { units: "miles" };
+
+const calcDistance = distance(from, to, options);
+console.log(calcDistance);
--- _test/script.js
+++ _test/script.js
@@ -1,1 +1,6 @@
-console.log("eh");
+const from = turf.point([-75.343, 39.984]);
+const to = turf.point([-75.534, 39.123]);
+const options = { units: "miles" };
+
+const calcDistance = turf.distance(from, to, options);
+console.log(calcDistance);
