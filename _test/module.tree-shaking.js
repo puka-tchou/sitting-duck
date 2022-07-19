fceb19d8a2e63ca9836e9aa57801f3107dd2f794
Gaspard [puka] <gaspard.bariller-drouet@sogefi-sig.com>
2022-07-19 11:29:16 +0200

    dev: add a tree-shaking test

---
+++ _test/module.tree-shaking.js
@@ -0,0 +1,9 @@
+// @MODULE
+import * as turf from "@turf/turf";
+
+const from = turf.point([-75.343, 39.984]);
+const to = turf.point([-75.534, 39.123]);
+const options = { units: "miles" };
+
+const calcDistance = turf.distance(from, to, options);
+console.log(calcDistance);
--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 import { minify } from "./index.js";
 
-minify(["_test/script.js", "_test/module.js"], ["_test/index.css"], "build");
+minify(
+  ["_test/script.js", "_test/module.js", "_test/module.tree-shaking.js"],
+  ["_test/index.css"],
+  process.argv[2]?.split("--")[1] ?? "build"
+);
