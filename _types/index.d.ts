ffedb0cbdb3ed448a6b399d288672e0f8902c117
Gaspard [puka] <gaspard.bariller-drouet@sogefi-sig.com>
2022-07-20 15:40:19 +0200

    ci: corrects the package build and publish

--- _types/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @param js La liste des fichiers Javascript à traiter, ou un glob.
- * @param css La liste des fichiers CSS à traiter, ou un glob.
- * @param mode Le mode à utiliser `'dev'` ou `'build'`. `'dev'` par défaut.
- * @see https://github.com/sindresorhus/globby#globbing-patterns
- */
-declare const main: (js: string[], css: string[], mode: string) => void;
-export { main as default };
-//# sourceMappingURL=index.d.ts.map
