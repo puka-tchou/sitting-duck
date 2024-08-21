8c11bd26d50998802b7a7519fee1543a8249623f
Puka Tchou <puka@duck.com>
2024-08-21 12:45:29 +0000

    misc: format files using prettier

--- __tests__/production.test.ts
+++ __tests__/production.test.ts
@@ -9,11 +9,16 @@ import path from "path";
 describe("production.ts", () => {
   describe("bundleWithSwc", () => {
     it("should bundle and minify a JavaScript file using SWC", (done) => {
-      bundleWithSwc("./fixtures/non-module.js", false, "./fixtures/non-module.min.js", 1);
+      bundleWithSwc(
+        "./fixtures/non-module.js",
+        false,
+        "./fixtures/non-module.min.js",
+        1,
+      );
       setTimeout(() => {
-        expect(fs.existsSync(path.join("./fixtures", "non-module.min.js"))).toBe(
-          true,
-        );
+        expect(
+          fs.existsSync(path.join("./fixtures", "non-module.min.js")),
+        ).toBe(true);
         done();
       }, 1000);
     });
@@ -39,7 +44,9 @@ describe("production.ts", () => {
   describe("production", () => {
     it("should bundle and minify JavaScript and CSS files", () => {
       production(["./fixtures/module.js", "./fixtures/example.css"], false);
-      expect(fs.existsSync(path.join("./fixtures", "module.min.js"))).toBe(true);
+      expect(fs.existsSync(path.join("./fixtures", "module.min.js"))).toBe(
+        true,
+      );
       expect(fs.existsSync(path.join("./fixtures", "example.min.css"))).toBe(
         true,
       );
