80a209afe2dd209c8a1d6a74e662fc9c4c934293
Puka Tchou <puka@duck.com>
2024-08-26 13:43:55 +0000

    tests: proper testing of options and utils files

--- __tests__/options.test.ts
+++ __tests__/options.test.ts
@@ -1,27 +1,44 @@
 import { esbuildOptions } from "../src/options";
 
-describe("options.ts", () => {
-  it("should export the correct esbuild options", () => {
-    expect(esbuildOptions).toEqual({
-      bundle: true,
-      format: "iife",
-      platform: "browser",
-      target: [],
-      legalComments: "linked",
-      loader: {
-        ".png": "file",
-        ".jpg": "file",
-        ".jpeg": "file",
-        ".webp": "file",
-        ".svg": "file",
-        ".gif": "file",
-        ".otf": "file",
-        ".ttf": "file",
-        ".woff": "file",
-        ".woff2": "file",
-        ".eot": "file",
-      },
-      assetNames: "assets/[ext]/[name]",
-    });
+describe("esbuildOptions Configuration", () => {
+  test("should set bundle to true", () => {
+    expect(esbuildOptions.bundle).toBe(true);
+  });
+
+  test('should set format to "iife"', () => {
+    expect(esbuildOptions.format).toBe("iife");
+  });
+
+  test('should set platform to "browser"', () => {
+    expect(esbuildOptions.platform).toBe("browser");
+  });
+
+  test("should initialize target as an empty array", () => {
+    expect(esbuildOptions.target).toEqual([]);
+  });
+
+  test('should set legalComments to "linked"', () => {
+    expect(esbuildOptions.legalComments).toBe("linked");
+  });
+
+  test("should configure the correct loaders for file types", () => {
+    const expectedLoaders = {
+      ".png": "file",
+      ".jpg": "file",
+      ".jpeg": "file",
+      ".webp": "file",
+      ".svg": "file",
+      ".gif": "file",
+      ".otf": "file",
+      ".ttf": "file",
+      ".woff": "file",
+      ".woff2": "file",
+      ".eot": "file",
+    };
+    expect(esbuildOptions.loader).toEqual(expectedLoaders);
+  });
+
+  test('should set assetNames to "assets/[ext]/[name]"', () => {
+    expect(esbuildOptions.assetNames).toBe("assets/[ext]/[name]");
   });
 });
--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,33 +1,47 @@
 import { isCSS, getminpath, isModule } from "../src/utils";
+import * as fs from "fs";
 
-describe("utils.ts", () => {
-  describe("isCSS", () => {
-    it("should return true for a CSS file", () => {
-      expect(isCSS("example.css")).toBe(true);
-    });
+jest.mock("fs");
 
-    it("should return false for a non-CSS file", () => {
-      expect(isCSS("example.js")).toBe(false);
-    });
+describe("Utils Tests", () => {
+  test("isCSS should return true for .css files", () => {
+    expect(isCSS("styles.css")).toBe(true);
+    expect(isCSS("index.html")).toBe(false);
   });
 
-  describe("getminpath", () => {
-    it("should add .min to the file path", () => {
-      expect(getminpath("example.js")).toBe("example.min.js");
-    });
-
-    it("should not add .min to a file without an extension", () => {
-      expect(getminpath("example")).toBe("example");
-    });
+  test("getminpath should return correct minified path", () => {
+    expect(getminpath("index.js")).toBe("index.min.js");
+    expect(getminpath("styles.css")).toBe("styles.min.css");
+    expect(getminpath("README")).toBe("README");
   });
 
-  describe("isModule", () => {
-    it("should return true for a module file", async () => {
-      expect(await isModule("./fixtures/module.js")).toBe(true);
-    });
+  test("isModule should resolve true if file contains @MODULE comment", async () => {
+    const mockStream = {
+      on: jest.fn().mockImplementation(function (event, callback) {
+        if (event === "data") callback("// @MODULE");
+        if (event === "close") callback();
+        return this;
+      }),
+      close: jest.fn(),
+    };
+    fs.createReadStream.mockReturnValue(mockStream);
 
-    it("should return false for a non-module file", async () => {
-      expect(await isModule("./fixtures/non-module.js")).toBe(false);
-    });
+    const result = await isModule("testfile.js");
+    expect(result).toBe(true);
+  });
+
+  test("isModule should resolve false if file does not contain @MODULE comment", async () => {
+    const mockStream = {
+      on: jest.fn().mockImplementation(function (event, callback) {
+        if (event === "data") callback('console.log("Hello World");');
+        if (event === "close") callback();
+        return this;
+      }),
+      close: jest.fn(),
+    };
+    fs.createReadStream.mockReturnValue(mockStream);
+
+    const result = await isModule("testfile.js");
+    expect(result).toBe(false);
   });
 });
